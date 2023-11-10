d718aa1fce117e14c4b6c12511e82e82c0a2d788
Moana Jubert <117283709+mjub@users.noreply.github.com>
2023-11-10 14:37:06 +0100

    Add the cartesian product of types

--- theories/Foundations/MLTT.v
+++ theories/Foundations/MLTT.v
@@ -31,5 +31,8 @@ Notation "∑ x .. y , B" := (Sigma _ (λ x, .. (Sigma _ (λ y, B)) ..))
   (at level 10, x binder, y binder, B at level 200,
   format "'[  ' '[  ' ∑  x  ..  y ']' ,  '/' B ']'").
 
+Notation "A × B" := (∑ _ : A, B)
+  (at level 40, right associativity).
+
 Notation "( x , .. , y , z )" := (pair x (.. (pair y z) ..))
   (at level 0, format "( '[' x ,  '/' .. ,  '/' y ,  '/' z ']' )").
